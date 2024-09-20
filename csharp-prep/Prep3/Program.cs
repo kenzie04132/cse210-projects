8a790ed3022a4cf6f21670314b57f57ec3df5309
kenzie04132 <mackenzie04132@byui.edu>
2024-09-20 16:54:39 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string answer = Console.ReadLine();
+        int magic_number = int.Parse(answer);
+
+        int guess = -1;
+
+        while (guess != magic_number)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magic_number)
+            {
+                Console.WriteLine("Guess lower.");
+            }
+
+            else if (guess < magic_number)
+            {
+                Console.WriteLine("Guess higher.");
+            }
+
+            else if (guess == magic_number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
