6152c718c4fd61f81c5f205f35e82c567d528b6c
kenzie04132 <mackenzie04132@byui.edu>
2024-09-20 17:56:50 -0600

    prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcomeMessage();
+
+        string name = prompt_name();
+        int number = prompt_number();
+
+        int squared_number = square_number(number);
+
+        result(name, squared_number);
+
+        static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string prompt_name()
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int prompt_number()
+        {
+            Console.Write("What is your favorite number? ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        static int square_number(int number)
+        {
+            int square = number * number;
+
+            return square;
+        }
+
+        static void result(string name, int square)
+        {
+            Console.WriteLine($"{name}, your favorite number squared is {square}.");
+        }
+
     }
 }
