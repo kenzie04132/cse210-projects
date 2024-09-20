5f2e0b8880c692d005b4c1fbcdc8eb01092adc77
kenzie04132 <mackenzie04132@byui.edu>
2024-09-20 16:36:17 -0600

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your number grade in your course? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        if (percent >= 90)
+        {
+            Console.WriteLine("You passed! You have an A in the course.");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("You passed! You have an B in the course.");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("You passed! You have an C in the course.");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Sorry, you did not pass. You have an D in the course.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. You have an F in the course.");
+        }
     }
 }
